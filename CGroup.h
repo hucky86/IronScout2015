cbedf389a189649cefe078231cc1aa9fee805ae4
Christopher Huck <christopherhuck@hotmail.com>
2015-01-08 19:55:26 +0100

    CGroup: Anlegen der Klasse CGroup für die Auswerte-Routinen

---
+++ CGroup.h
@@ -0,0 +1,33 @@
+// IRON SCOUT AUSWERTUNG
+//************************************************************************
+// Klasse:          CGroup
+// 
+// Beschreibung:    Abstrakte Basisklasse für gespeicherte Daten einer 
+//                  Gruppe für die Ausertung. Abgeleitete Klassen CRunner
+//                  und CStation.
+//                  
+//************************************************************************
+
+#ifndef CGROUP_H
+#define CGROUP_H
+ 
+class CGroup
+{
+ 
+public:
+  
+// Konstruktoren:
+    
+// Memberfunktionen:
+
+  private:
+
+//Membervariablen:
+    
+//Memberfunktionen:
+    
+protected:
+ 
+};
+ 
+#endif // CGROUP_H
