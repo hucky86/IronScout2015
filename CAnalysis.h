243b2a67127966f9b96ac9b65b949e6a4b4b3559
Christopher Huck <christopherhuck@hotmail.com>
2015-01-11 10:46:21 +0100

    CAnalysis: Implementierung einer Klasse für Auswerteroutinen

---
+++ CAnalysis.h
@@ -0,0 +1,37 @@
+// IRON SCOUT AUSWERTUNG
+//************************************************************************
+// Klasse:          CAnalysis
+// 
+// Beschreibung:    Stellt Schnittstelle für Auswerteroutinen zur
+//                  Verfügung.
+// 
+//************************************************************************
+
+#ifndef CANALYSIS_H
+#define CANALYSIS_H
+
+#include <iostream>
+#include <fstream>
+
+ using namespace std;
+ 
+class CAnalysis
+{
+ 
+public:
+  
+// Konstruktoren:
+    
+// Memberfunktionen:
+
+  private:
+
+//Membervariablen:
+
+//Memberfunktionen:
+    
+protected:
+ 
+};
+
+#endif // CANALYSIS_H
