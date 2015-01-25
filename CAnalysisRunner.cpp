f9c91646432f0fbb3405a8bd99df5ae55be42c7f
Christopher Huck <christopherhuck@hotmail.com>
2015-01-25 16:47:29 +0100

    CAnalysisRunner: Abgeleitete Klasse zur Ausertung der Läufergruppen

---
+++ CAnalysisRunner.cpp
@@ -0,0 +1,6 @@
+#include "CAnalysisRunner.h"
+
+CAnalysisRunner::CAnalysisRunner()
+{
+
+}
---
+++ CAnalysisRunner.h
@@ -0,0 +1,37 @@
+// IRON SCOUT AUSWERTUNG
+//************************************************************************
+// Klasse:          CAnalysisRunner
+// 
+// Beschreibung:    Stellt Schnittstelle für Auswerteroutinen zur
+//                  Verfügung. Abgeleitet von CAnalysis zur Auswertung der
+//                  Läufergruppen.
+// 
+//************************************************************************
+
+
+#ifndef CANALYSISRUNNER_H
+#define CANALYSISRUNNER_H
+
+#include "CAnalysis.h"
+
+class CAnalysisRunner : public CAnalysis
+{
+ 
+public:
+  
+// Konstruktoren:
+  CAnalysisRunner();
+    
+// Memberfunktionen:
+
+  private:
+
+//Membervariablen:
+
+//Memberfunktionen:
+    
+protected:
+ 
+};
+
+#endif // CANALYSISRUNNER_H
