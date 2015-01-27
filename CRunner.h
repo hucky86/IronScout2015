5a7e6bc3fed727c43bed7d9b0ea17fe9f162f043
Christopher Huck <christopherhuck@hotmail.com>
2015-01-27 19:03:32 +0100

    CRunner: Hinzufügen einer abgeleiteten Klasse CRunner von CGroup.

---
+++ CRunner.h
@@ -0,0 +1,39 @@
+// IRON SCOUT AUSWERTUNG
+//************************************************************************
+// Klasse:          CRunner
+// 
+// Beschreibung:    Speichert Daten zur Auswertung einer Läufergruppe.
+//                  Abgeleitet von CGroup.
+//                  
+//************************************************************************
+
+#ifndef CRUNNER_H
+#define CRUNNER_H
+
+#include <iostream>
+#include <fstream>
+
+#include "CGroup.h"
+ 
+using namespace std;
+ 
+class CRunner : public CGroup
+{
+ 
+public:
+  
+// Konstruktoren:
+    
+// Memberfunktionen:
+
+private:
+
+//Membervariablen:
+
+//Memberfunktionen:
+    
+protected:
+ 
+};
+ 
+#endif // CRUNNER_H
